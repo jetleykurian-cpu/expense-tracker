75311d25feb788e13ad66938c8d435f81fb788e7
jetley <jetleykurian@gmail.com>
2026-05-06 16:27:37 +0530

    fix: lowercase template for linux deployement

--- expenses/auth_views.py
+++ expenses/auth_views.py
@@ -22,7 +22,7 @@ def login_view(request):
             return redirect('home')
     else:
         form = AuthenticationForm()
-    return render(request, 'expenses/Login.html', {'form': form})
+    return render(request, 'expenses/login.html', {'form': form})
 
 def logout_view(request):
     logout(request)
