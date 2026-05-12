cb1398c706b40b4f0a93109cbbf807d5f70d4800
jetley <jetleykurian@gmail.com>
2026-05-12 16:00:06 +0530

    feat: refactor to class-based views with crud

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from . import views
+from .views import ExpenseView, ExpenseListView, ExpenseCreateView, ExpenseUpdateView, ExpenseDeleteView
 from . import auth_views
 from .api_views import ExpenseListCreateView, ExpenseDetailView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # main views
+    path('', ExpenseListView.as_view(), name='home'),
+    path('create/', ExpenseCreateView.as_view(), name='expense-create'),
+    path('<int:pk>/edit/', ExpenseUpdateView.as_view(), name='expense-edit'),
+    path('<int:pk>/delete/', ExpenseDeleteView.as_view(), name='expense-delete'),
     path('register/', auth_views.register_view, name='register'),
     path('login/', auth_views.login_view, name='login'),
-    path('login/', auth_views.logout_view, name='logout'),
+    path('logout/', auth_views.logout_view, name='logout'),
 
      # API endpoints
      path('api/expenses/', ExpenseListCreateView.as_view(), name ='api-expenses'),
--- expenses/views.py
+++ expenses/views.py
@@ -1,25 +1,79 @@
+from django.views import View
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Expense
 from .forms import ExpenseForm
-@login_required(login_url ='login')
-def home(request):
-    if request.method == 'POST':
+
+class UserOwnerMixin:
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+class ExpenseView(View):
+    template_name = 'expenses/home.html'
+
+    def get(self, request):
+        expenses = Expense.objects.filter(user=request.user).order_by('-date')
+        total = sum(e.amount for e in expenses)
+        form = ExpenseForm()
+        return render(request, self.template_name, {
+            'expenses': expenses,
+            'total': total, 
+            'form': form
+        })
+    
+    def post(self, request):
         form = ExpenseForm(request.POST)
         if form.is_valid():
-            expense = form.save(commit=False)
+            expense = form.save(commit = False)
             expense.user = request.user
             expense.save()
             return redirect('home')
-    else:
+        expenses = Expense.objects.filter(user=request.user).order_by('-date')
+        total = sum(e.amount for e in expenses)
         form = ExpenseForm()
-    expenses = Expense.objects.filter(user=request.user).order_by('-date')
-    total = sum(e.amount for e in expenses)
+        return render(request, self.template_name, {
+            'expenses': expenses,
+            'total': total, 
+            'form': form
+        })
 
-    context = {
-        "expenses": expenses,
-        "total": total,
-        'form': form,
-    }
-    return render(request, "expenses/home.html", context)
+class ExpenseListView(LoginRequiredMixin, ListView):
+    model = Expense
+    template_name = 'expenses/home.html'
+    context_object_name = 'expenses'
 
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user).order_by('-date')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total'] = sum(e.amount for e in self.get_queryset())
+        context['form'] = ExpenseForm()
+        return context
+    
+class ExpenseCreateView(LoginRequiredMixin, CreateView):
+    model = Expense
+    form_class = ExpenseForm
+    template_name = 'expenses/home.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class ExpenseUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    model = Expense
+    form_class = ExpenseForm
+    template_name = 'expenses/edit.html'
+    success_url = reverse_lazy('home')
+
+    
+    
+class ExpenseDeleteView(LoginRequiredMixin, UserOwnerMixin, DeleteView):
+    model = Expense
+    template_name = 'expenses/delete.html'
+    success_url = reverse_lazy('home')
+
+    
